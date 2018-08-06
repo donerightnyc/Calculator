d9cc6ed53f7a732165ae0aa878ea94acb3a85aca
Felipe <felhenao@gmail.com>
2018-08-06 15:03:03 -0400

    MVP done

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Calculator-
 Calculator with average button styled using CSS Grid
+
+
+## Sample
+<img src="./images/calculator.png" />
---
+++ script/calculations.js
@@ -0,0 +1,22 @@
+function getAverage() {
+	secondCalculator.inputBox.value = eval(secondCalculator.inputBox.value)/
+	secondCalculator.inputBox.value.split("+").length;
+}
+
+
+
+
+
+
+
+
+
+// function getAverage() {
+// 	var arr = [];
+// 	for (var i = 0; i <= numbers.lenght; i++) {
+// 		var addedItems = arr.push(i);
+// 	}
+// 	console.log(arr);
+// }
+
+// getAverage(2);
